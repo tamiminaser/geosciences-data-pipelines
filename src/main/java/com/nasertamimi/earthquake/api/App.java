a2add0f3ce443403cfed59696cc391a5b4a3223d
tamiminaser <tamimi.naser@yahoo.com>
2023-02-27 21:53:27 -0600

    initial commit

---
+++ src/main/java/com/nasertamimi/earthquake/api/App.java
@@ -0,0 +1,13 @@
+package com.nasertamimi.earthquake.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
