39233ee6eab45eab63500053c28c94fbd10f531b
tamiminaser <tamimi.naser@yahoo.com>
2023-03-19 23:13:33 -0500

    writing the response to an output file

--- src/main/java/com/nasertamimi/earthquake/api/App.java
+++ src/main/java/com/nasertamimi/earthquake/api/App.java
@@ -1,6 +1,12 @@
 package com.nasertamimi.earthquake.api;
 
 import java.net.HttpURLConnection;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+
 
 public class App {
     public static void main( String[] args ) {
@@ -8,9 +14,15 @@ public class App {
             GeoConn geoConn = new GeoConn();
             Request request = new Request();
 
-            String response = request.perform(geoConn.usgs());
+            LocalDate localDate = LocalDate.now();
+            String startDate = localDate.minusDays(1).toString();
+            String endDate = localDate.toString();
+            String response = request.perform(geoConn.usgs(startDate, endDate));
 
             System.out.println(response);
+
+            Path path = Paths.get(String.format("/tmp/earthquakeApi/output_%s.json", LocalDate.now()));
+            Files.writeString(path, response,  StandardCharsets.UTF_8);
 
         } catch (Exception e) {
             System.out.println("An error occured: " + e.getMessage());
--- src/main/java/com/nasertamimi/earthquake/api/GeoConn.java
+++ src/main/java/com/nasertamimi/earthquake/api/GeoConn.java
@@ -2,12 +2,15 @@ package com.nasertamimi.earthquake.api;
 
 import java.net.URL;
 import java.net.HttpURLConnection;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class GeoConn {
     public GeoConn(){
     }
-    public HttpURLConnection usgs() throws Exception{
-        String endpoint = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2023-02-27&endtime=2023-02-28";
+    public HttpURLConnection usgs(String startDate, String endDate) throws Exception{
+
+        String endpoint = String.format("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=%s&endtime=%s", startDate, endDate);
 
         // creating a connection
         URL url = new URL(endpoint);
