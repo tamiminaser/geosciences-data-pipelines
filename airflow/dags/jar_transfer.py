6d97f66ca2cd04780e3510006040595c69236c59
tamiminaser <tamimi.naser@yahoo.com>
2023-05-02 22:56:19 -0500

    adding jar transfer dag

--- airflow/dags/jar_transfer.py
+++ airflow/dags/jar_transfer.py
@@ -7,14 +7,14 @@ from airflow.operators.bash import BashOperator
 S3_BUCKET_DAG = Variable.get("S3_BUCKET_DAG")
 
 dag = DAG(
-        dag_id="dag_transfer",
+        dag_id="jar_transfer",
         schedule='* * * * *',
         start_date=datetime(2023, 4, 21),
         catchup=False,
 )
 
-transfer_dags_from_s3 = BashOperator(
+transfer_jars_from_s3 = BashOperator(
     task_id="syncing",
-    bash_command=f"aws s3 sync s3://{S3_BUCKET_DAG}/dags /home/ubuntu/airflow/dags --exact-timestamps",
+    bash_command=f"aws s3 sync s3://{S3_BUCKET_DAG}/jars /home/ubuntu/airflow/resources/jars --exact-timestamps",
     dag=dag,
 )
