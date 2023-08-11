315a85081413b4f524028636298e3415787fa0ad
tamiminaser <tamimi.naser@yahoo.com>
2023-08-10 23:32:36 -0500

    Throwing IOException 2

--- src/main/scala/com/nasertamimi/geosciences/datapipelines/tasks/USGSEarthquakeDomainEventTask.scala
+++ src/main/scala/com/nasertamimi/geosciences/datapipelines/tasks/USGSEarthquakeDomainEventTask.scala
@@ -4,37 +4,39 @@ import com.nasertamimi.geosciences.datapipelines.USGS.earthquake.USGSEarthquakeD
 import com.nasertamimi.geosciences.datapipelines.core.{Downloader, Task}
 import org.apache.logging.log4j.{LogManager, Logger}
 
+import java.io.IOException
 import java.nio.file.Path
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter;
 
 object USGSEarthquakeDomainEventTask {
 
-    def main(args: Array[String]): Unit = {
-            val logger = LogManager.getLogger(this.getClass)
-            logger.info("Start USGS Earthquake Task.")
-            logger.info(s"Start Date: ${System.getProperty("start_date")}")
-            logger.info(s"End Date: ${System.getProperty("end_date")}")
+  @throws(classOf[IOException])
+  def main(args: Array[String]): Unit = {
+          val logger = LogManager.getLogger(this.getClass)
+          logger.info("Start USGS Earthquake Task.")
+          logger.info(s"Start Date: ${System.getProperty("start_date")}")
+          logger.info(s"End Date: ${System.getProperty("end_date")}")
 
-            val startDate = LocalDate.parse(System.getProperty("start_date"))
-            val endDate = LocalDate.parse(System.getProperty("end_date"))
+          val startDate = LocalDate.parse(System.getProperty("start_date"))
+          val endDate = LocalDate.parse(System.getProperty("end_date"))
 
-            val downloader: USGSEarthquakeDownloader = new USGSEarthquakeDownloader()
+          val downloader: USGSEarthquakeDownloader = new USGSEarthquakeDownloader()
 
-            var runDate = startDate
-            var nextDate = runDate.plusDays(1)
+          var runDate = startDate
+          var nextDate = runDate.plusDays(1)
 
-            while (runDate.isBefore(endDate) || runDate.isEqual(endDate)) {
-                val runDateStr: String = runDate.format(DateTimeFormatter.ISO_LOCAL_DATE)
-                val nextDateStr: String = nextDate.format(DateTimeFormatter.ISO_LOCAL_DATE)
+          while (runDate.isBefore(endDate) || runDate.isEqual(endDate)) {
+              val runDateStr: String = runDate.format(DateTimeFormatter.ISO_LOCAL_DATE)
+              val nextDateStr: String = nextDate.format(DateTimeFormatter.ISO_LOCAL_DATE)
 
-                logger.info(String.format("Run Date is %s", runDateStr))
-                val downloadedPath = downloader.download(runDateStr, nextDateStr)
-                logger.info(String.format("Download completed for %s.", runDateStr))
-                logger.info(String.format("Download Path is: %s", String.valueOf(downloadedPath)))
+              logger.info(String.format("Run Date is %s", runDateStr))
+              val downloadedPath = downloader.download(runDateStr, nextDateStr)
+              logger.info(String.format("Download completed for %s.", runDateStr))
+              logger.info(String.format("Download Path is: %s", String.valueOf(downloadedPath)))
 
-                runDate = nextDate
-                nextDate = nextDate.plusDays(1)
-            }
-    }
+              runDate = nextDate
+              nextDate = nextDate.plusDays(1)
+          }
+  }
 }
