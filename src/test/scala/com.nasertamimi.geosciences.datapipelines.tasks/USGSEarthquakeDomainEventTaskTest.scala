f77a559c0bf2470fd971981686970edb800c207f
tamiminaser <tamimi.naser@yahoo.com>
2024-06-12 00:35:23 -0500

    adding scalatest

---
+++ src/test/scala/com.nasertamimi.geosciences.datapipelines.tasks/USGSEarthquakeDomainEventTaskTest.scala
@@ -0,0 +1,10 @@
+package com.nasertamimi.geosciences.datapipelines.tasks
+
+import org.scalatest.FunSuite
+
+class USGSEarthquakeDomainEventTaskTest extends FunSuite{
+
+  test("test1") {
+    true
+  }
+}
